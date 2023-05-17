3b260cb1feed87155704e5df0ae9fe821929da56
Sichu0725 <choihc8511@gmail.com>
2023-05-17 15:41:25 +0900

    feat:cors issue solve

--- server_start.py
+++ server_start.py
@@ -6,7 +6,8 @@ def start_fastapi():
 
 def start_react():
   os.chdir("simple_translate_front")
-  os.system("npx serve -s build -l 80")
+  os.system("npm start")
+  # os.system("npx serve -s build -l 80")
 
 if __name__ == "__main__":
   start_fastapi()
