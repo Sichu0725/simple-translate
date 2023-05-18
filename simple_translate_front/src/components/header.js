2ec64aae56286aaff0e95bb7ba0f9bc09c4a2949
Sichu0725 <choihc8511@gmail.com>
2023-05-18 20:38:24 +0900

    feat:index html lang change to 'ko' & header.js img add alt

--- simple_translate_front/src/components/header.js
+++ simple_translate_front/src/components/header.js
@@ -3,7 +3,7 @@ import sc_logo from '../assets/symbol-solid.png'
 export const Header = () => {
   return (
     <div className="header">
-      <img src={sc_logo} />{' '}
+      <img src={sc_logo} alt="로고" />{' '}
       <h1>
         <span>GBSW</span> 학생용 번역기
       </h1>
